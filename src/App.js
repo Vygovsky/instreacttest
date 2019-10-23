4dbe3048e204ef2e7aef0b50b410be611fc069da
Roman_v <vigmol@ukr.net>
2019-10-23 16:45:34 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from './component/Header/Header';
+
+
+function App() {
+    return (
+        <div className="App">
+            <Header/>
+            {/*     <Feed/>*/}
+        </div>
+    );
+}
+
+export default App;
